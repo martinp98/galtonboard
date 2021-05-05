2b638546992ff9f31fe0c65f24fac04d87fc395e
martinp98 <62560904+martinp98@users.noreply.github.com>
2021-05-05 18:27:42 +0200

    Delete presentation2 directory

--- presentation2/index.js
+++
@@ -1,7 +0,0 @@
-var number = document.getElementById("numberOfBalls");
-var output = document.getElementById("demo");
-output.innerHTML = number.value;
-
-slider.oninput = function() {
-    output.innerHTML = this.value;
-}
